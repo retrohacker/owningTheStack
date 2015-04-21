4aa7616c54cbdbbba7273d7425848e8b11617a6f
William Blankenship <william.jblankenship@gmail.com>
2015-04-21 13:22:06 -0500

    dockeri.co -> mongodb

--- gimmick1.sh
+++ gimmick1.sh
@@ -5,6 +5,6 @@ port_start=44000
 
 for i in `seq 1 $1`; do
   echo "Starting container $i on $port_start"
-  docker run -dp $port_start:8888 wblankenship/dockeri.co:latest
+  docker run -dp $port_start:8888 mongo
   port_start=$(($port_start+1))
 done
